609a840a1b82fcfab53e28fe8124a7f96542fc42
‘yoon-jisung’ <hmsj2380@gmail.com>
2025-01-30 01:43:00 +0900

    Chore: 프로젝트 라이브러리 설치
    - 스타일링 "tailwindCSS", "tailwind-merge" , "class-variance-authority", "clsx" 추가
      - 컬러 팔레트 설정
      - autoprefixer
      - tailwind-merge 사용 분기처리 util 함수 추가
    - 분기처리   "ts-pattern" 추가
    - mock api  "msw" 추가
    - 아이콘 "@radix-ui/react-icons", "lucide-react"
    - 상태관리 "zustand", "@tanstack/react-query"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,45 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      maxWidth: {
+        container: 'var(--max-width)',
+      },
+      colors: {
+        mint: {
+          900: 'var(--mint-900)',
+          700: 'var(--mint-700)',
+        },
+        midnight: {
+          900: 'var(--midnight-900)',
+          100: 'var(--midnight-100)',
+        },
+        gray: {
+          700: 'var(--gray-700)',
+          500: 'var(--gray-500)',
+          400: 'var(--gray-400)',
+          300: 'var(--gray-300)',
+          200: 'var(--gray-200)',
+          100: 'var(--gray-100)',
+          50: 'var(--gray-50)',
+          10: 'var(--gray-10)',
+        },
+        red: 'var(--red)',
+        blue: 'var(--blue)',
+      },
+      lineHeight: {
+        sm: 'var(--line-height-sm)',
+        md: 'var(--line-height-md)',
+        lg: 'var(--line-height-lg)',
+      },
+      transitionTimingFunction: {
+        primary: 'var(--cubic-bezier-primary)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
